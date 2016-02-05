a3e357c1479c9bb2070893c4874e3f2bc9a56c9c
zenoamaro <zenoamaro@gmail.com>
2016-02-06 00:54:30 +0100

    Disabled history when `recordHistory: false`

--- dist/jquery.fullpage.js
+++ dist/jquery.fullpage.js
@@ -2256,14 +2256,6 @@
         function setUrlHash(url){
             if(options.recordHistory){
                 location.hash = url;
-            }else{
-                //Mobile Chrome doesn't work the normal way, so... lets use HTML5 for phones :)
-                if(isTouchDevice || isTouch){
-                    window.history.replaceState(undefined, undefined, '#' + url);
-                }else{
-                    var baseUrl = window.location.href.split('#')[0];
-                    window.location.replace( baseUrl + '#' + url );
-                }
             }
         }
 
